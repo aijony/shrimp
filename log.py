c84b2d2c40addc82bff3ae410f20a5413353fd38
Aidan Nyquist <contact@aidannyquist.com>
2018-02-25 12:05:14 -0600

    Fix log.py

--- log.py
+++ log.py
@@ -3,12 +3,12 @@ import os
 import datetime
 import time
 
+
 def log(message, name):
     dr = 'cache'
     ts = time.time()
     if not os.path.exists(dr):
         os.makedirs(dr)
-        stamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M')
-        with open(dir + '/' + name, 'a') as name:
-            f.write(stamp + ' - ' + message + '\n')
-
+    stamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M')
+    with open(dir + '/' + name, 'a') as f:
+        f.write(stamp + ' - ' + message + '\n')
